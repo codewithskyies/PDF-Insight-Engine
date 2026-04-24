c7692db449774d88bd7a4446b089e5cb0a30ea65
Aakash gupta <aakashsahuu0188@gmail.com>
2026-04-24 12:20:16 +0530

    Add files via upload

---
+++ DB.py
@@ -0,0 +1,45 @@
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+load_dotenv()
+
+from langchain_core.documents import Document
+
+docs = [
+    Document(page_content="Python is widely used in Artificial Intelligence.", metadata={"source": "AI_book"}),
+    Document(page_content="Pandas is used for data analysis in Python.", metadata={"source": "DataScience_book"}),
+    Document(page_content="Neural networks are used in deep learning.", metadata={"source": "DL_book"}),
+]
+
+# embedding_model = OpenAIEmbeddings()
+
+
+
+
+
+embedding_model=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+
+vectorstore = Chroma.from_documents(
+    documents = docs,
+    embedding= embedding_model,
+    persist_directory= "chroma-db"
+)
+
+result = vectorstore.similarity_search("what is used for data analysis?",k=2)
+
+for r in result:
+    print(r.page_content)
+    print(r.metadata)
+
+retriver = vectorstore.as_retriever()
+
+docs = retriver.invoke("Explain deep learning")
+
+for d in docs:
+    print(d.page_content)
